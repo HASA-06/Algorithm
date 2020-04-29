f440a303343c5a2ffec87f3878fc2b3b87ddb486
HASA-06 <hasadev92@gmail.com>
2020-04-29 14:11:03 +0900

    0429 book

---
+++ book/prime_number.js
@@ -0,0 +1,24 @@
+function isPrime(n) {
+    // 시간복잡도로 n을 가지는 isPrime 함수
+    if(n <= 1) return false;
+
+    for(var i = 2; i < n; i++) {
+        if(n % i == 0) return false;
+    }
+
+    return true;
+}
+
+function isPrime(n) {
+    // 시간복잡도로 sqrt(n)을 가지는 개선된 isPrime 함수
+    if(n <= 1) return false;
+    if(n <= 3) return true;
+
+    if(n % 2 == 0 || n % 3 == 0) return false;
+
+    for(var i = 5; i * i <= n; i += 6) {
+        if((n % i == 0) && (n % (i + 2) == 0)) return false;
+    }
+
+    return true;
+}
